40b88e6e87e2aa1feb9986ec7a6782af846a0422
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-10-19 16:17:47 -0500

    Import CSS to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './components/App';
+import css from './style.css';
 //allows you to render the initial view (possibly a webpack issue?)
 window.React = React;
 
