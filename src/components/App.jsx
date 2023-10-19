8dac97eb5b9f242d98801cdfeb85c4e15365bf30
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2023-10-18 21:20:04 -0500

    Populate Window with Note Title and Body

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react";
 
 const App = () => {
+
+  const [notes, setNotes] = useState(null);
   
   useEffect(()=> {
     fetch('http://localhost:3000/pomonotes')
     .then((res)=> res.json())
-    .then((data)=> console.log(data))
-  })
-
-  const [notes, setNotes] = useState('Loading notes...');
-
+    .then((data)=> setNotes(data));
+  });
 
   return (
-    <div>Hello World!</div>
+    <div className="noteBody">
+      <h2>Pomonotes:</h2>
+      {notes && notes.map(note => {
+        return (
+          <div> 
+            <h3>{note.title}</h3>
+            <h4>{note.noteBody}</h4>
+          </div>
+        )
+      })}
+    </div>
   )
 };
 
